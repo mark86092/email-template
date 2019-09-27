0c4bbd781923a81d66ae22e5edc92019127de003
Wei-Ming Chen <barry800414@gmail.com>
2019-09-27 23:59:15 +0800

    feat: experiecne-view-log-notification email tempalte

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const account_verify = require('./account_verify');
+const experience_view_log_notification = require('./experience_view_log_notification');
 
 module.exports = {
   account_verify,
+  experience_view_log_notification,
 }
